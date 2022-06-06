6a87ea4d8f047f8af188192a3dd0ef76299d4e8f
nessux <33452102+nessux@users.noreply.github.com>
2022-06-06 19:07:18 +0200

    Updated city scene
    Updated render scene script and city scene to use budget and budget type variables instead of constants.

--- mitsuba/render_scene.sh
+++ mitsuba/render_scene.sh
@@ -1,17 +1,18 @@
 scene_dir=scenes
 scene=city
-integrator=path
-spp=256
+integrator=guided_path
+budgetType=seconds
+budget=3600
 echo
 echo
-echo Render reference for $scene with $integrator with $spp spp
+echo Render $scene with $integrator with $budget $budgetType
 echo
 echo Starting at $(date)
-time ./dist/mitsuba -z ${scene_dir}/${scene}/${scene}.xml -o ${scene_dir}/${scene}/${scene}_mitsuba_${integrator}_${spp}spp.pfm -D sampleCount=${spp} -r 1800
+time ./dist/mitsuba -z ${scene_dir}/${scene}/${scene}.xml -o ${scene_dir}/${scene}/${integrator}_${budget}_${budgetType}.exr -D budgetType=${budgetType} -D budget=${budget} -r 1800
 
 echo 
 echo
-echo Finished $scene with $integrator and $spp spp at $(date)
+echo Finished $scene with $integrator and $budget $budgetType at $(date)
 echo
 echo
 
